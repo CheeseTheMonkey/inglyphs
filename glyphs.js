e5f94f896f4875246651edce6ee103d7f3023dfc
Chris Clarke <christopher_clarke@trendmicro.co.uk>
2015-02-26 12:14:34 +0000

    Update glyph list

--- glyphs.js
+++ glyphs.js
@@ -1,506 +1,577 @@
 var glyphs = {
     'abandon'     : {
         'name': 'Abandon',
+        'alt': '',
         'vertices': [2,4,5,6,8,10]
     },
     'adapt'       : {
         'name': 'Adapt',
+        'alt': '',
         'vertices': [1,6,5,7]
     },
     'advance'     : {
         'name': 'Advance',
+        'alt': '',
         'vertices': [0,3,8]
     },
     'again'       : {
-        'name': 'Again/Repeat',
+        'name': 'Again',
+        'alt': 'Repeat',
         'vertices': [8,3,6,5,4,7]
     },
     'all'         : {
         'name': 'All',
+        'alt': '',
         'vertices': [0,1,8,10,9,2,0]
     },
     'answer'      : {
         'name': 'Answer',
+        'alt': '',
         'vertices': [3,4,7,5]
     },
     'attack'      : {
-        'name': 'Attack/War',
+        'name': 'Attack',
+        'alt': 'War',
         'vertices': [8,3,0,4,9]
     },
     'avoid'       : {
-        'name': 'Avoid/Struggle',
+        'name': 'Avoid',
+        'alt': '',
         'vertices': [1,0,4,2,7]
     },
     'barrier'     : {
-        'name': 'Barrier/Obstacle',
+        'name': 'Barrier',
+        'alt': 'Obstacle',
         'vertices': [0,5,7,9]
     },
     'begin'       : {
         'name': 'Begin',
+        'alt': '',
         'vertices': [0,6,10,7]
     },
     'being'       : {
-        'name': 'Being/Human',
+        'name': 'Being',
+        'alt': '',
         'vertices': [3,4,7,10,6,3]
     },
     'body'        : {
-        'name': 'Body/Shell',
+        'name': 'Body',
+        'alt': '',
         'vertices': [3,4,5,3]
     },
     'breathe'     : {
         'name': 'Breathe',
+        'alt': '',
         'vertices': [1,3,5,4,2]
     },
     'calibration' : {
         'name': 'Calibration Grid',
+        'alt': '',
         'vertices': []
     },
     'capture'     : {
         'name': 'Capture',
+        'alt': '',
         'vertices': [10,8,6,5,7,2]
     },
     'change'      : {
-        'name': 'Change/Modify',
+        'name': 'Change',
+        'alt': '',
         'vertices': [6,5,10,7]
     },
     'chaos'       : {
-        'name': 'Chaos/Disorder',
+        'name': 'Chaos',
+        'alt': '',
         'vertices': [8,1,0,2,4,5,6,10]
+    },
+    'civilization'  : {
+        'name': 'Civilization',
+        'alt': 'City',
+        'vertices': [1,3,6,7,4,2]
     },
     'clear'       : {
         'name': 'Clear',
+        'alt': '',
         'vertices': [0,5,10]
     },
     'close_all'   : {
         'name': 'Close All',
+        'alt': '',
         'vertices': [0,1,8,10,9,2,0,5,10]
     },
     'complex'     : {
         'name': 'Complex',
+        'alt': '',
         'vertices': [4,3,5,8]
     },
     'conflict'    : {
         'name': 'Conflict',
+        'alt': '',
         'vertices': [8,3,6,7,4,9]
     },
     'consequence' : {
         'name': 'Consequence',
+        'alt': '',
         'vertices': [1,3,6,7,9]
     },
     'contemplate' : {
         'name': 'Contemplate',
+        'alt': '',
         'vertices': [0,2,9,10,6,3,5,4]
     },
     'contract'    : {
-        'name': 'Contract/Reduce',
+        'name': 'Contract',
+        'alt': 'Reduce',
         'vertices': [7,4,9]
     },
     'courage'     : {
         'name': 'Courage',
+        'alt': '',
         'vertices': [8,3,6,7]
     },
     'create'      : {
-        'name': 'Create/Creation',
+        'name': 'Create',
+        'alt': '',
         'vertices': [8,6,5,4,2]
     },
     'creativity'  : {
         'name': 'Creativity',
+        'alt': '',
         'vertices': [1,3,5,10,6,1]
-    },
-    'create_mind' : {
-        'name': 'Creativity/Mind',
-        'vertices': [6,8,1,3,5,7,9,2,4]
     },
     'danger'      : {
         'name': 'Danger',
+        'alt': '',
         'vertices': [0,3,5,10]
     },
     'data'        : {
-        'name': 'Data/Signal',
+        'name': 'Data',
+        'alt': '',
         'vertices': [0,4,5,6,10]
     },
     'defend'      : {
         'name': 'Defend',
+        'alt': '',
         'vertices': [1,6,10,7,2]
     },
     'destination' : {
         'name': 'Destination',
+        'alt': '',
         'vertices': [2,9,10]
     },
     'destiny'     : {
         'name': 'Destiny',
+        'alt': '',
         'vertices': [3,5,4,7,6,10]
     },
-    'destruction' : {
-        'name': 'Destruction',
+    'destroy' : {
+        'name': 'Destroy',
+        'alt': '',
         'vertices': [1,3,5,7,9]
     },
     'deteriorate' : {
-        'name': 'Deteriorate/Erode',
+        'name': 'Deteriorate',
+        'alt': '',
         'vertices': [3,5,6,8]
     },
     'die'         : {
         'name': 'Die',
+        'alt': '',
         'vertices': [8,6,5,7,9]
     },
     'difficult'   : {
         'name': 'Difficult',
+        'alt': '',
         'vertices': [2,4,7,5,6]
     },
     'discover'    : {
         'name': 'Discover',
+        'alt': '',
         'vertices': [2,9,10,8]
     },
     'distance'    : {
-        'name': 'Distance/Outside',
+        'name': 'Distance',
+        'alt': 'Outside',
         'vertices': [0,1,8]
     },
     'easy'        : {
         'name': 'Easy',
+        'alt': '',
         'vertices': [4,5,6,10]
     },
     'end'         : {
-        'name': 'End/Close',
+        'name': 'End',
+        'alt': '',
         'vertices': [0,2,7,10,5,0]
     },
     'enlightened' : {
         'name': 'Enlightened',
+        'alt': '',
         'vertices': [3,4,5,3,0,2,9,10]
-    },
-    'enlightened_b' : {
-        'name': 'Enlightened',
-        'vertices': [3,4,5,3,0,2,9,10,8]
     },
     'equal'       : {
         'name': 'Equal',
+        'alt': '',
         'vertices': [6,3,4,7]
     },
     'escape'      : {
         'name': 'Escape',
+        'alt': '',
         'vertices': [0,2,4,3,6]
     },
     'evolution'  : {
-        'name': 'Evolution/Success',
+        'name': 'Evolution',
+        'alt': 'Success',
         'vertices': [0,5,3,6]
     },
     'failure'     : {
         'name': 'Failure',
+        'alt': '',
         'vertices': [0,5,4,7]
     },
     'fear'        : {
         'name': 'Fear',
+        'alt': '',
         'vertices': [3,4,7,2]
     },
     'follow'      : {
         'name': 'Follow',
+        'alt': '',
         'vertices': [0,4,2,9]
     },
     'forget'      : {
         'name': 'Forget',
+        'alt': '',
         'vertices': [6,8]
     },
     'future'      :{
-        'name': 'Future/Forward-Time',
+        'name': 'Future',
+        'alt': '',
         'vertices': [2,4,7,9]
     },
     'gain'        : {
         'name': 'Gain',
+        'alt': '',
         'vertices': [1,6]
-    },
-    'government'  : {
-        'name': 'Government/City',
-        'vertices': [1,3,6,7,4,2]
     },
     'grow'        : {
         'name': 'Grow',
+        'alt': '',
         'vertices': [8,3,6]
     },
     'harm'        : {
         'name': 'Harm',
+        'alt': '',
         'vertices': [5,4,0,3,5,7,9]
     },
     'harmony'     : {
-        'name': 'Harmony/Peace',
+        'name': 'Harmony',
+        'alt': 'Peace',
         'vertices': [0,3,5,7,10,6,5,4,0]
     },
     'have'        : {
         'name': 'Have',
+        'alt': '',
         'vertices': [10,6,5,7]
     },
     'help'        : {
         'name': 'Help',
+        'alt': '',
         'vertices': [1,3,5,6,7]
     },
     'hide'        : {
         'name': 'Hide',
+        'alt': '',
         'vertices': [3,4,2,7,6]
     },
-    'i_me_self'   : {
-        'name': 'I/Me/Self',
-        'vertices': [3,4,10,3]
+    'human': {
+        'name': 'Human',
+        'alt': '',
+        'vertices': [3,4,7,10,6,3]
+    },
+    'idea' : {
+        'name': 'Idea',
+        'alt': 'Thought',
+        'vertices': [6,8,1,3,5,7,9,2,4]
     },
     'ignore'      : {
         'name': 'Ignore',
+        'alt': '',
         'vertices': [7,9]
     },
     'imperfect'   : {
         'name': 'Imperfect',
-        'vertices': [4,5,6,3,5]
+        'alt': '',
+        'vertices': [5,3,6,4]
     },
     'improve'     : {
         'name': 'Improve',
+        'alt': '',
         'vertices': [2,4,5,7]
     },
     'impure'      : {
         'name': 'Impure',
+        'alt': '',
         'vertices': [5,3,6,5,10]
     },
     'journey'     : {
         'name': 'Journey',
+        'alt': '',
         'vertices': [2,4,5,3,1,8,10]
-    },
-    'knowledge'   : {
-        'name': 'Knowledge',
-        'vertices': [3,5,4,10,3]
     },
     'lead'        : {
         'name': 'Lead',
+        'alt': '',
         'vertices': [0,1,8,6,10]
     },
     'legacy'      : {
         'name': 'Legacy',
+        'alt': '',
         'vertices': [8,6,3,1,0,2,4,7,9]
     },
     'less'        : {
         'name': 'Less',
+        'alt': '',
         'vertices': [3,5,4]
     },
     'liberate'    : {
         'name': 'Liberate',
+        'alt': '',
         'vertices': [0,2,4,5,3,8]
     },
     'lie'         : {
         'name': 'Lie',
+        'alt': '',
         'vertices': [6,3,5,7,4,5]
     },
-    'live_again'  : {
-        'name': 'Live Again',
-        'vertices': [2,4,5,6,3,8]
-    },
     'lose'        : {
-        'name': 'Lose/Loss',
+        'name': 'Lose',
+        'alt': '',
         'vertices': [2,7]
     },
     'message'     : {
         'name': 'Message',
+        'alt': '',
         'vertices': [2,7,5,3,8]
     },
     'mind'        : {
-        'name': 'Mind/Idea/Thought',
+        'name': 'Mind',
+        'alt': '',
         'vertices': [3,5,10,6,3]
     },
     'more'        : {
         'name': 'More',
+        'alt': '',
         'vertices': [6,5,7]
-    },
-    'mystery'     : {
-        'name': 'Mystery',
-        'vertices': [1,3,4,0,3,6]
     },
     'nature'      : {
         'name': 'Nature',
+        'alt': '',
         'vertices': [8,6,3,4,7,9]
     },
     'new'         : {
         'name': 'New',
+        'alt': '',
         'vertices': [4,7,9]
     },
-    'no'          : {
-        'name': 'No/Not/Absent/Inside',
+    'not'         : {
+        'name': 'Not',
+        'alt': 'Inside',
         'vertices': [3,4,7]
     },
     'nourish'     : {
         'name': 'Nourish',
+        'alt': '',
         'vertices': [5,6,8,10,5]
     },
     'old'         : {
         'name': 'Old',
+        'alt': '',
         'vertices': [1,3,6]
     },
     'open'        : {
-        'name': 'Open/Accept',
+        'name': 'Open',
+        'alt': 'Accept',
         'vertices': [6,7,10,6]
     },
     'open_all'    : {
         'name': 'Open All',
+        'alt': '',
         'vertices': [10,8,1,0,2,9,10,6,7,10]
-    },
-    'opening'     : {
-        'name': 'Opening/Doorway',
-        'vertices': [1,3,4,2,9,7,6,8,1]
     },
     'past'        : {
         'name': 'Past',
+        'alt': '',
         'vertices': [1,3,6,8]
     },
     'path'        : {
         'name': 'Path',
+        'alt': '',
         'vertices': [0,5,6,8]
     },
     'perfection'  : {
-        'name': 'Perfection/Balance',
+        'name': 'Perfection',
+        'alt': 'Balance',
         'vertices': [0,5,6,8,10,9,7,5]
     },
     'perspective' : {
         'name': 'Perspective',
+        'alt': '',
         'vertices': [8,6,5,4,0,3,5,7,9]
+    },
+    'portal'     : {
+        'name': 'Portal',
+        'alt': '',
+        'vertices': [1,3,4,2,9,7,6,8,1]
     },
     'potential'   : {
         'name': 'Potential',
+        'alt': '',
         'vertices': [0,5,7,9,2]
     },
-    'presence'    : {
-        'name': 'Presence',
-        'vertices': [6,3,5,4,7,10,6,7]
-    },
     'present'     : {
-        'name': 'Present/Now',
+        'name': 'Present',
+        'alt': 'Now',
         'vertices': [3,6,7,4]
     },
     'pure'        : {
-        'name': 'Pure/Purity',
+        'name': 'Pure',
+        'alt': '',
         'vertices': [0,5,4,7,5]
     },
     'pursue'      : {
-        'name': 'Pursue/Aspiration',
+        'name': 'Pursue',
+        'alt': '',
         'vertices': [1,3,0,4]
-    },
-    'pursue_chase': {
-        'name': 'Pursue/Chase',
-        'vertices': [0,5,3,6,8]
     },
     'question'    : {
         'name': 'Question',
+        'alt': '',
         'vertices': [0,4,3,6]
     },
     'react'       : {
         'name': 'React',
+        'alt': '',
         'vertices': [4,3,5,7,9]
     },
     'rebel'       : {
         'name': 'Rebel',
+        'alt': '',
         'vertices': [1,6,5,4,2,9]
     },
     'recharge'    : {
         'name': 'Recharge',
+        'alt': 'Repair',
         'vertices': [0,1,3,5,0]
     },
-    'resist'      : {
-        'name': 'Resist/Resistance',
+    'resistance'      : {
+        'name': 'Resistance',
+        'alt': 'Struggle',
         'vertices': [6,10,5,0,3,4]
-    },
-    'resist_b'      : {
-        'name': 'Resist/Resistance',
-        'vertices': [7,10,5,0,3,4]
     },
     'restraint'   : {
         'name': 'Restraint',
+        'alt': '',
         'vertices': [1,3,5,7,9,10]
     },
     'retreat'     : {
         'name': 'Retreat',
+        'alt': '',
         'vertices': [0,4,9]
     },
     'safety'      : {
         'name': 'Safety',
+        'alt': '',
         'vertices': [8,3,4,9]
     },
     'save'        : {
-        'name': 'Save/Rescue',
+        'name': 'Save',
+        'alt': '',
         'vertices': [2,7,5,6]
     },
     'see'         : {
         'name': 'See',
+        'alt': '',
         'vertices': [0,3]
     },
-    'seek'        : {
-        'name': 'Seek/Search',
+    'search'        : {
+        'name': 'Search',
+        'alt': '',
         'vertices': [5,4,3,6,7]
     },
     'self'        : {
         'name': 'Self',
+        'alt': '',
         'vertices': [8,10,9]
     },
-    'separate'    : {
-        'name': 'Separate',
+    'seperate'    : {
+        'name': 'Seperate',
+        'alt': '',
         'vertices': [1,3,6,5,4,7,9]
     },
-    'shaper'      : {
-        'name': 'Shaper/Collective',
+    'shapers'      : {
+        'name': 'Shapers',
+        'alt': '',
         'vertices': [8,6,3,0,4,7,9]
-    },
-    'shaper_being': {
-        'name': 'Shaper+Being',
-        'vertices': [8,6,3,0,4,3,6,10,7,4,7,9]
     },
     'share'       : {
         'name': 'Share',
+        'alt': '',
         'vertices': [9,7,6,8,10]
     },
     'simple'      : {
         'name': 'Simple',
+        'alt': '',
         'vertices': [6,7]
     },
     'soul'        : {
         'name': 'Soul/Spirit',
+        'alt': '',
         'vertices': [4,5,10,7,4]
     },
     'stability'   : {
-        'name': 'Stability/Stay',
+        'name': 'Stability',
+        'alt': 'Stay',
         'vertices': [8,6,7,9]
     },
     'strong'      : {
         'name': 'Strong',
+        'alt': '',
         'vertices': [3,4,7,6]
     },
     'together'    : {
         'name': 'Together',
+        'alt': '',
         'vertices': [5,3,4,5,6,8]
     },
     'truth'       : {
         'name': 'Truth',
+        'alt': '',
         'vertices': [3,5,7,4,5,6,3]
     },
     'use'         : {
         'name': 'Use',
+        'alt': '',
         'vertices': [2,7,5]
     },
-    'victory'     : {
-        'name': 'Victory',
-        'vertices': [0,3,10,4,0]
-    },
     'want'        : {
-        'name': 'Want/Desire',
+        'name': 'Want',
+        'alt': '',
         'vertices': [8,6,10,7]
-    },
-    'we'          : {
-        'name': 'We/Us',
-        'vertices': [3,4,10]
     },
     'weak'        : {
         'name': 'Weak',
+        'alt': '',
         'vertices': [1,3,4,7]
     },
     'worth'       : {
         'name': 'Worth',
+        'alt': '',
         'vertices': [1,6,5,7,2]
     },
     'xm'          : {
         'name': 'XM(Exotic Matter)',
+        'alt': '',
         'vertices': [3,4,7,5,6,3]
-    },
-    'you'         : {
-        'name': 'You/Other',
-        'vertices': [0,6,7,0]
     },
 }
